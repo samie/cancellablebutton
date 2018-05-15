622f9f8138967c8a2cba9311268588ab9e6d0ba6
Sami Ekblad <sami@vaadin.com>
2018-05-15 15:37:22 +0300

    Fixed wrong widgetset name

--- src/test/java/org/vaadin/addonhelpers/Config.java
+++ src/test/java/org/vaadin/addonhelpers/Config.java
@@ -2,7 +2,7 @@ package org.vaadin.addonhelpers;
 
 import com.vaadin.annotations.Widgetset;
 
-@Widgetset("org.vaadin.cancelablebutton.DelayedbuttonWidgetset")
+@Widgetset("org.vaadin.cancelablebutton.CancellableButtonWidgetset")
 public class Config {
 
 }
